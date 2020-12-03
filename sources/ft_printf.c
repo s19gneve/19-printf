997a4fb7722ef0e5345f18296ae9b7ee64ab3a0d
rballant <ballantromain@gmail.com>
2020-12-03 17:03:15 +0100

    psudio

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -1,1 +1,21 @@
 
+
+void printf(char *s, ...)
+{
+	va_list args;
+	int		numb;
+	int		i;
+	char 	*s2;
+
+	numbs = counts_args(s);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == "%")
+			s2 += add_argv();
+		else
+			s2[j] = s[i];
+		i++;
+	}
+	ft_putstr_fd(s2, 1);
+}
