927bb3b0bc644f62ecaf08df8324e18e8904a221
gneve <gneve@student.s19.be>
2020-12-03 08:32:05 -0800

    gregoire

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -1,5 +1,24 @@
 #include "ft.h"
 
+static int counts_args(char *s) {
+	int i;
+	int a;
+
+	i = 0;
+	a = 0;
+	if (s[0] == 0)
+		return (0);
+	while (s && s[i + 1] != 0 )
+	{
+		if (s[i] == "%" && s[i + 1] != "%")
+		{
+			a++;
+		}
+		i++;
+	}
+	return (a);
+}
+
 int ft_printf(char *s, ...)
 {
 	va_list args;
