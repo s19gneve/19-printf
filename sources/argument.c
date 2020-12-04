574e1998c092fc95f89e610382829c3cc618346b
gneve <gneve@student.s19.be>
2020-12-04 04:32:00 -0800

    gregoire

---
+++ sources/argument.c
@@ -0,0 +1,28 @@
+char *(char flags)
+{
+	if (flags == "c")
+		return (1);
+	if (flags == "s")
+		return (2);
+	if (flags == "p")
+		return (3);
+	if (flags == "d")
+		return (4);
+	if (flags == "i")
+		return (5);
+	if (flags == "u")
+		return (1);
+	if (flags == "x")
+		return (1);
+	if (flags == "X")
+		return (1);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
