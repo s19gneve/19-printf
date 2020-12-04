b4ca34d5b928b974c58c81b4b6ea5f97a415ed55
gneve <gneve@student.s19.be>
2020-12-04 04:39:13 -0800

    gregoire

--- headers/ft.h
+++ headers/ft.h
@@ -6,13 +6,16 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 08:12:52 by gneve             #+#    #+#             */
-/*   Updated: 2020/12/03 08:16:57 by gneve            ###   ########.fr       */
+/*   Updated: 2020/12/04 04:38:51 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
 # include <stdarg.h>
+# include "stddef.h"
+# include "unistd.h"
+# include "stdlib.h"
 
 int	ft_printf(char *s, ...);
 #endif
---
+++ sources/ft_itoa.c
@@ -0,0 +1,60 @@
+#include "ft.h"
+
+static int			lenght(long nb)
+{
+	int		len;
+
+	len = 0;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		len++;
+	}
+	if (nb == 0)
+	{
+		return (1);
+	}
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		len++;
+	}
+	return (len);
+}
+
+static unsigned int	ft_isnegative(int n, unsigned char is_negative)
+{
+	if (is_negative)
+		return (-n);
+	else
+		return (n);
+}
+
+char				*ft_itoa(int n)
+{
+	char			*ret;
+	unsigned char	is_negative;
+	unsigned char	len;
+	unsigned int	un;
+
+	is_negative = n < 0;
+	un = ft_isnegative(n, is_negative);
+	len = lenght(n);
+	if (!(ret = malloc(len + 1)))
+		return (NULL);
+	if (n == 0)
+	{
+		ret[0] = '0';
+		ret[1] = '\0';
+		return (ret);
+	}
+	ret[len] = '\0';
+	while (len--)
+	{
+		ret[len] = un % 10 + '0';
+		un /= 10;
+	}
+	if (is_negative)
+		ret[0] = '-';
+	return (ret);
+}
