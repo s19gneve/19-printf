1e5bdbb837098c4517aa9dc7a8b530c78b5a05ee
rballant <ballantromain@gmail.com>
2020-12-04 14:42:59 +0100

    clean up

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -8,26 +8,6 @@ static void *arg_type(va_list args, char *s, int *i)
 		return (ft_itoa(va_arg(args, int)));
 }
 
-static int counts_args(char *s)
-{
-	int i;
-	int a;
-
-	i = 0;
-	a = 0;
-	if (s[0] == 0)
-		return (0);
-	while (s && s[i + 1] != 0 )
-	{
-		if (s[i] == '%' && s[i + 1] != '%')
-		{
-			a++;
-		}
-		i++;
-	}
-	return (a);
-}
-
 int ft_printf(char *s, ...)
 {
 	va_list args;
