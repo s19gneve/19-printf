4c25c1689db945be2c7d63fdf016d124868b67b1
gneve <gneve@student.s19.be>
2020-12-04 04:48:02 -0800

    g

--- headers/ft.h
+++ headers/ft.h
@@ -6,14 +6,13 @@
 /*   By: gneve <gneve@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/03 08:12:52 by gneve             #+#    #+#             */
-/*   Updated: 2020/12/04 04:42:17 by gneve            ###   ########.fr       */
+/*   Updated: 2020/12/04 04:47:49 by gneve            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
 # include <stdarg.h>
-# include "libft.a"
 
 int	ft_printf(char *s, ...);
 #endif
